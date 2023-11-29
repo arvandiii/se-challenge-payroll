5b6846225441afa78b3d5ea714fe284d44de4e6e
Alireza Arvandi <alireza.arv.mail@gmail.com>
2023-11-29 13:08:13 -0700

    unit tests added

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,11 +1,24 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT
+const multer = require('multer')
+
+const upload = multer({ dest: './tmp/' })
+
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+    res.send({response: 'up'})
+})
+
+app.post('/upload/report', upload.single('report'), function (req, res, next) {
+    // req.file is the `avatar` file
+    // req.body will hold the text fields, if there were any
+
+    console.log(req.file);
+    console.log(req.body);
+    res.send({response: 'uploaded'})
 })
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port ${port}`)
 })
